11e10c1e1e1a58da65bfcd8eb970438948ff3a23
VrishabM <vrishabmsharma@gmail.com>
2024-03-08 20:52:10 +0530

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, NgZone  } from "@angular/core";
+
+declare var annyang: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.less"]
 })
 export class AppComponent {
-  title = 'speech-recognition';
+  capturedText: string = '';
+  private isRecording: boolean = false;
+
+  constructor(private ngZone: NgZone) {}
+
+  ngOnInit() {
+    // Initialize annyang
+    if (annyang) {
+      annyang.debug();
+      annyang.addCallback('result', (phrases: string[]) => {
+        this.ngZone.run(() => {
+          this.capturedText = phrases[0];
+        });
+      });
+    }
+  }
+
+  startRecording() {
+    if (annyang && !this.isRecording) {
+      this.isRecording = true;
+      annyang.start();
+    }
+  }
+
+  stopRecording() {
+    if (annyang && this.isRecording) {
+      this.isRecording = false;
+      annyang.abort();
+    }
+  }
 }
